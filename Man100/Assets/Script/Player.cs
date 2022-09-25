b342071a592bd71ab94c722776a922a842388210
lixm <lxmcf110@163.com>
2022-09-25 22:10:33 +0800

    新增判断是否是碰撞在floor上边

--- Man100/Assets/Script/Player.cs
+++ Man100/Assets/Script/Player.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
+
+    // 创建目前脚下阶梯的变量
+    GameObject currentFloor;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,11 +34,23 @@ public class Player : MonoBehaviour
     {
         if (collision.transform.tag == "Normal")
         {
-            Debug.Log("碰到(Object)Normal");
+            if (collision.contacts[0].normal == new Vector2 (0f,1f))
+            {
+                Debug.Log("碰到Normal");
+                currentFloor = collision.gameObject;
+            }
         } else if (collision.transform.tag == "Nails")
         {
-            Debug.Log("碰到(Object)Nails");
-        }         
+            if (collision.contacts[0].normal == new Vector2(0f, 1f))
+            {
+                Debug.Log("碰到Nails");
+                currentFloor = collision.gameObject;
+            }
+        } else if (collision.transform.tag == "Ceiling")
+        {
+            Debug.Log("碰到天花板");
+            currentFloor.GetComponent<BoxCollider2D>().enabled = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
